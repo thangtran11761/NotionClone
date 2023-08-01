2d46053d0875bec3537db1b39529a27538373877
- <haiconbo2020@gmail.com>
2023-08-01 20:10:41 +0700

    rename

--- src/services/TaskService.js
+++
@@ -1,9 +0,0 @@
-import httpRequestJson from "./common";
-
-export const getTasks = async () => {
-  return httpRequestJson("GET", "tasks");
-};
-
-export const addTask = async (body) => {
-  return httpRequestJson("POST", "tasks", body);
-};
