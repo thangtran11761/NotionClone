8680375d089cab2d390a2f634c05f02cc9679ddd
- <haiconbo2020@gmail.com>
2023-08-01 20:07:35 +0700

    add PageProvider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Main from "./layout/Main.jsx";
+import { PageProvider } from "./context/Page.context";
 
 function App() {
-  return <Main></Main>;
+  return (
+    <PageProvider>
+      <Main />
+    </PageProvider>
+  );
 }
 
 export default App;
