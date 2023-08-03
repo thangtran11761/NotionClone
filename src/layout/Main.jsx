7410ffbcb1b9342ff89674135eaaa58ff5157c84
- <haiconbo2020@gmail.com>
2023-08-03 14:52:36 +0700

    change layout

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,24 +1,26 @@
-import React, { memo } from 'react'
+import React, { memo, useContext } from 'react'
 import { Col, Row, Layout, Space } from 'antd';
 
-import ListOfTasks from '../components/Task/ListOfTasks'
-import SideBar from '../components/SideBar';
+import Task from '../components/Task/Task'
+import SideBar from '../components/SideBar/SideBar';
 
 import classes from './style.module.css'
+import { PageContext } from '../context/Page.context';
 
 const { Sider, Content } = Layout;
 
 const Main = () => {
+    const pageCtx = useContext(PageContext)
+
     return (
         <Layout className={classes.layout}>
             <Sider style={{ backgroundColor: '#fff' }}>
                 <SideBar />
             </Sider>
             <Layout>
-                <ListOfTasks />
+                {pageCtx.page?.type === 'tasks' && (< Task page={pageCtx.page} />)}
             </Layout>
         </Layout>
-
     )
 }
 
