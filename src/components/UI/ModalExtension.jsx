8f8f3573d3253a60cc38c682c454cf0afbef6dc1
- <haiconbo2020@gmail.com>
2023-08-05 19:02:39 +0700

    add modal backdrop

---
+++ src/components/UI/ModalExtension.jsx
@@ -0,0 +1,35 @@
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
+
+import classes from "./ModalExtension.module.css";
+
+const Backdrop = (props) => {
+    return <div className={classes.backdrop} onClick={props.onCloseModal}></div>;
+};
+
+const ModalOverlay = (props) => {
+    return (
+        <div className={classes.modal}>
+            <div className={classes.content}>{props.children}</div>
+        </div>
+    );
+};
+
+const portalElement = document.getElementById("overlays");
+
+const ModalExtension = (props) => {
+    return (
+        <Fragment>
+            {ReactDOM.createPortal(
+                <Backdrop onCloseModal={props.onCloseModal} />,
+                portalElement
+            )}
+            {ReactDOM.createPortal(
+                <ModalOverlay>{props.children}</ModalOverlay>,
+                portalElement
+            )}
+        </Fragment>
+    );
+};
+
+export default ModalExtension;
