616eb90bf08e47bd371ae3ceb231d6daeef54c2a
- <haiconbo2020@gmail.com>
2023-09-04 01:16:04 +0700

    add logic fnc add page

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,29 @@
 import { createStore } from "redux";
 
 import { addPages, removePages, editPages } from "../services/PageService";
+import { addTask } from "../services/TaskService";
+import { addText } from "../services/TextServices";
+import { addSchedule } from "../services/ScheduleServices";
+import { addProject } from "../services/ProjectServices";
 
 const initialState = { activity: true };
 
 const pageReducer = (state = initialState, action) => {
   if (action.type === "addPage") {
-    addPages(action.data).then((res) => console.log(res));
+    addPages(action.data).then((res) => {
+      if (res.type === "task") {
+        addTask({ column: [], idPage: res.id, name: res.name });
+      }
+      if (res.type === "text") {
+        addText({ idPage: res.id, name: res.name });
+      }
+      if (res.type === "schedule") {
+        addSchedule({ idPage: res.id, name: res.name });
+      }
+      if (res.type === "project") {
+        addProject({ idPage: res.id, name: res.name });
+      }
+    });
     return { activity: !state.activity };
   }
 
