b0d2a07bb9a4f91a5064f1eaf386ae47b10658b7
- <haiconbo2020@gmail.com>
2023-10-12 00:11:36 +0700

    add Draggable

--- src/components/Task/TaskItem.jsx
+++ src/components/Task/TaskItem.jsx
@@ -1,12 +1,29 @@
-import React, { memo } from 'react'
+import React, { memo, useState } from 'react'
+// import { Draggable } from 'react-beautiful-dnd';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+
+
 import classes from './Task.module.css'
 
-const TaskItem = memo
-    (({ todo }) => {
+const TaskItem =
+    memo(({ todo, index }) => {
         return (
-            <div className={classes['todo-item']}>
-                {todo.name}
-            </div>
+            <Draggable
+                key={todo.id}
+                draggableId={todo.id.toString()}
+                index={index}
+            >
+                {(provided) => (
+                    <div
+                        className={classes['todo-item']}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        ref={provided.innerRef}
+                    >
+                        {todo.name ? todo.name : <p className={classes['todo-item__untitled']}>Untitled</p>}
+                    </div>
+                )}
+            </Draggable>
         )
     })
 
