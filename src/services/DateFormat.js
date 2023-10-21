57a83c799afc1d0b7dc31ebd40b0f7b902aca739
- <haiconbo2020@gmail.com>
2023-10-22 02:09:08 +0700

    add spreadDate - convertStringToDateTime

--- src/services/DateFormat.js
+++ src/services/DateFormat.js
@@ -24,3 +24,22 @@ export function dateFormat(date) {
 
   return "" + year + month + day + hours + minutes + seconds;
 }
+
+export function spreadDate(date) {
+  let year = date?.slice(0, 4);
+  let month = date?.slice(4, 6);
+  let day = date?.slice(6, 8);
+
+  return `${day}-${month}-${year}`;
+}
+
+export const convertStringToDateTime = (string) => {
+  const year = string.slice(0, 4);
+  const month = string.slice(4, 6);
+  const day = string.slice(6, 8);
+  const hour = string.slice(8, 10);
+  const minute = string.slice(10, 12);
+  const second = string.slice(12, 14);
+
+  return new Date(year, month - 1, day, hour, minute, second);
+};
